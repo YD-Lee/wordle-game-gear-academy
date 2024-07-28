7c219ee288253aa36fa01aad77fbd14aa2866121
YD-Lee <ydlee096@gmail.com>
2024-07-28 22:48:01 +0800

    Wordle Program and template for Game Session Program.

---
+++ game-session/src/lib.rs
@@ -0,0 +1,22 @@
+#![no_std]
+use gstd::{exec, msg, prelude::*, ActorId};
+
+#[no_mangle]
+extern "C" fn init() {
+    // todo
+}
+
+#[no_mangle]
+extern "C" fn handle() {
+    // todo
+}
+
+#[no_mangle]
+extern "C" fn handle_reply() {
+    // todo
+}
+
+#[no_mangle]
+extern "C" fn state() {
+    // todo
+}
