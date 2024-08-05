16929f63cd67530af897cf97779e4384e6642347
YD-Lee <ydlee096@gmail.com>
2024-08-05 18:54:07 +0800

    Change gear to version 1.4.1, and update tests.

--- game-session/src/lib.rs
+++ game-session/src/lib.rs
@@ -1,19 +1,16 @@
 #![no_std]
-use gstd::{exec, msg, prelude::*, debug, ActorId, collections::HashMap};
+use gstd::{exec, msg, prelude::*, debug, ActorId, collections::HashMap, MessageId};
 use wordle_io::{Action, Event};
 use game_session_io::*;
 
 static mut GAME_SESSION_STATE: Option<GameSessionState> = None;
 const MAX_CHECK_COUNT: u8 = 6;
-#[cfg(test)]
-const CHECK_GAME_STATUS_DELAY: u32 = 10;
-#[cfg(not(test))]
 const CHECK_GAME_STATUS_DELAY: u32 = 200;
 
 #[no_mangle]
 extern "C" fn init() {
     let wordle_program = msg::load().expect("Unable to decode init");
-    debug!("wordle program id: {}", wordle_program);
+    debug!("wordle program id: {:x?}", wordle_program);
 
     unsafe {
         GAME_SESSION_STATE = Some(GameSessionState {
@@ -24,64 +21,64 @@ extern "C" fn init() {
     msg::reply(SessionEvent::Initialized, 0).expect("Unable to reply init");
 }
 
-fn handle_start_game() {
+fn start_game() {
     let state = unsafe {GAME_SESSION_STATE.as_mut().expect("GAME_SESSION_STATE is not initialized")};
     let user = msg::source();
     if !state.user_to_session.contains_key(&user) {
         state.user_to_session.insert(user, Session {
             start_block: 0,
             check_count: 0,
-            msg_ids: (0.into(), 0.into()),
+            msg_ids: (MessageId::zero(), MessageId::zero()),
             status: SessionStatus::StartGameWaiting,
             result: SessionResult::Ongoing,
         });
     }
 
     let session: &mut Session = state.user_to_session.get_mut(&user).unwrap();
-    debug!("handle_start_game: status is {:x?}", session.status);
+    debug!("start_game: status is {:x?}", session.status);
     match &session.status {
         SessionStatus::StartGameWaiting | SessionStatus::CheckWordWaiting => {
             let msg_id: gstd::MessageId = msg::send(state.wordle_program, Action::StartGame { user }, 0)
-                .expect("handle_start_game: error in sending `Action::StartGame`");
+                .expect("start_game: error in sending `Action::StartGame`");
             session.msg_ids = (msg_id, msg::id());
             session.status = SessionStatus::StartGameSent;
 
-            debug!("handle_start_game: `StartGame` wait");
+            debug!("start_game: `StartGame` wait");
             exec::wait();
         },
         SessionStatus::ReplyReceived(recv_event) => {
             if let SessionEvent::GameStarted = recv_event {
                 session.start_block = exec::block_height();
                 session.check_count = 0;
-                session.msg_ids = (0.into(), 0.into());
+                session.msg_ids = (MessageId::zero(), MessageId::zero());
                 session.status = SessionStatus::CheckWordWaiting;
                 session.result = SessionResult::Ongoing;
                 msg::reply(SessionEvent::GameStarted , 0).expect("Error in sending `GameStarted` reply");
-                debug!("send delayed message, program={}, user={}", exec::program_id(), user);
+                debug!("start_game: send delayed message, program={:x?}, user={:x?}", exec::program_id(), user);
                 msg::send_delayed(exec::program_id(), SessionAction::CheckGameStatus { user }, 0, CHECK_GAME_STATUS_DELAY)
-                    .expect("handle_start_game: error in sending `SessionAction::CheckGameStatus`");
+                    .expect("start_game: error in sending `SessionAction::CheckGameStatus`");
             } else {
-                panic!("handle_start_game: invalid received event: {:x?}", recv_event);
+                panic!("start_game: invalid received event");
             }
         },
-        _ => panic!("handle_start_game: wrong status, {:x?}", session.status),
+        _ => panic!("start_game: wrong status"),
     }
 }
 
-fn handle_check_word(word: String) {
-    let state = unsafe {GAME_SESSION_STATE.as_mut().expect("GAME_SESSION_STATE is not initialized")};
+fn check_word(word: String) {
+    let state = unsafe {GAME_SESSION_STATE.as_mut().expect("check_word: GAME_SESSION_STATE is not initialized")};
     let user = msg::source();
     if !state.user_to_session.contains_key(&user) {
-        panic!("handle_check_word: non-existing user");    
+        panic!("check_word: non-existing user");    
     }
 
     let session: &mut Session = state.user_to_session.get_mut(&user).unwrap();
-    debug!("handle_check_word: status is {:x?}", session.status);
+    debug!("check_word: status is {:x?}", session.status);
 
     match &session.status {
         SessionStatus::CheckWordWaiting => {
             if word.len() != 5 || !word.chars().all(|c| c.is_lowercase()) {
-                panic!("handle_check_word: invalid word, {}", word);
+                panic!("check_word: invalid word");
             }
             
             session.check_count += 1;
@@ -89,31 +86,31 @@ fn handle_check_word(word: String) {
                 session.status = SessionStatus::StartGameWaiting;
                 session.result = SessionResult::Lose;
                 msg::reply(SessionEvent::GameOver { result: SessionResult::Lose }, 0)
-                    .expect("handle_check_word: error in replying `SessionEvent::GameOver`");
+                    .expect("check_word: error in replying `SessionEvent::GameOver`");
             } else {
                 let msg_id = msg::send(state.wordle_program, Action::CheckWord { user, word }, 0)
-                    .expect("handle_check_word: error in sending `Action::CheckWord`");
+                    .expect("check_word: error in sending `Action::CheckWord`");
                 session.msg_ids = (msg_id, msg::id());
                 session.status = SessionStatus::CheckWordSent;
 
-                debug!("handle_check_word: `CheckWord` wait");
+                debug!("check_word: `CheckWord` wait");
                 exec::wait();
             }
         },
         SessionStatus::ReplyReceived(recv_event) => {
             if let SessionEvent::WordChecked { correct_positions, contained_in_word } = recv_event {
 
-                session.msg_ids = (0.into(), 0.into());
+                session.msg_ids = (MessageId::zero(), MessageId::zero());
                 if correct_positions.len() == 5 {
                     session.status = SessionStatus::StartGameWaiting;
                     session.result = SessionResult::Win;
-                    msg::reply(SessionEvent::GameOver { result: SessionResult::Lose, }, 0)
-                        .expect("handle_check_word: error in replying `GameOver(Win)`");
+                    msg::reply(SessionEvent::GameOver { result: SessionResult::Win, }, 0)
+                        .expect("check_word: error in replying `GameOver(Win)`");
                 } else if session.check_count >= 6 {
                     session.status = SessionStatus::StartGameWaiting;
                     session.result = SessionResult::Lose;
                     msg::reply(SessionEvent::GameOver { result: SessionResult::Lose }, 0)
-                        .expect("handle_check_word: error in replying `GameOver(Lose)`");
+                        .expect("check_word: error in replying `GameOver(Lose)`");
                 } else {
                     let event = SessionEvent::WordChecked {
                         correct_positions: correct_positions.to_vec(),
@@ -121,53 +118,54 @@ fn handle_check_word(word: String) {
                     };
                     session.status = SessionStatus::CheckWordWaiting;
                     session.result = SessionResult::Ongoing;
-                    msg::reply(event, 0).expect("handle_check_word: error in replying `WordChecked`");
+                    msg::reply(event, 0).expect("check_word: error in replying `WordChecked`");
                 }
             } else {
-                panic!("handle_check_word: invalid ReplyReceived event: {:x?}", recv_event);
+                panic!("check_word: invalid ReplyReceived event");
             }
         },
-        _ => panic!("handle_check_word: wrong status ({:x?})", session.status),
+        _ => panic!("check_word: wrong status"),
     }
 }
 
-fn handle_check_game_status(user: &ActorId) {
-    debug!("handle_check_game_status");
+fn check_game_status(user: &ActorId) {
+    debug!("check_game_status");
     let state = unsafe {GAME_SESSION_STATE.as_mut()
-        .expect("handle_check_game_status: GAME_SESSION_STATE is not initialized")};
+        .expect("check_game_status: GAME_SESSION_STATE is not initialized")};
     if !state.user_to_session.contains_key(user) {
-        panic!("handle_check_game_status: non-existing user");    
+        panic!("check_game_status: non-existing user");    
     }
 
     let session: &mut Session = state.user_to_session.get_mut(user).unwrap();
-    debug!("handle_check_game_status: block_height={}, start_block={}", exec::block_height(), session.start_block);
-    if exec::block_height() > session.start_block + CHECK_GAME_STATUS_DELAY && session.result == SessionResult::Ongoing {
+    debug!("check_game_status: block_height={}, start_block={}", exec::block_height(), session.start_block);
+    if exec::block_height() >= session.start_block + CHECK_GAME_STATUS_DELAY && session.result == SessionResult::Ongoing {
         session.result = SessionResult::Lose;
         session.status = SessionStatus::StartGameWaiting;
         msg::send(*user, SessionEvent::GameOver { result: SessionResult::Lose, }, 0)
-            .expect("handle_check_game_status: error in sending `GameOver(Lose)`");
+            .expect("check_game_status: error in sending `GameOver(Lose)`");
     }
 }
 
 #[no_mangle]
 extern "C" fn handle() {
-    let action: SessionAction = msg::load().expect("handle: unable to decode handle");
-    debug!("handle: action is {:x?}", &action);
+    debug!("---handle---");
+    let action: SessionAction = msg::load().expect("Unable to decode handle");
+    debug!("action is {:x?}", &action);
 
     match &action {
-        SessionAction::StartGame => handle_start_game(),
-        SessionAction::CheckWord { word } => handle_check_word(word.to_string()),
-        SessionAction::CheckGameStatus { user } => handle_check_game_status(&user),
+        SessionAction::StartGame => start_game(),
+        SessionAction::CheckWord { word } => check_word(word.to_string()),
+        SessionAction::CheckGameStatus { user } => check_game_status(&user),
     }
 }
 
 #[no_mangle]
 extern "C" fn handle_reply() {
-    debug!("handle_reply");
+    debug!("---handle_reply---");
     let state = unsafe {GAME_SESSION_STATE.as_mut().expect("GAME_SESSION_STATE is not initialized")};
     let reply_to = msg::reply_to().expect("Failed to query reply_to data");
     let reply_message: Event = msg::load().expect("Unable to decode wordle's reply message");
-    debug!("handle_reply: reply_message {:x?}", reply_message);
+    debug!("Reply message: {:x?}", reply_message);
 
     match &reply_message {
         Event::GameStarted { user } => {
--- game-session/tests/game_session_test.rs
+++ game-session/tests/game_session_test.rs
@@ -11,31 +11,29 @@ fn setup() -> System {
     let sys = System::new();
     sys.init_logger();
 
-
     let wordle = Program::from_file(&sys, "../target/wasm32-unknown-unknown/debug/wordle.wasm");
     let game_session = Program::from_file(&sys, "../target/wasm32-unknown-unknown/debug/game_session.wasm");
 
-    let user_id: ActorId = USER1.into();
+    // let user_id: ActorId = USER1.into();
     let wordle_id: ActorId = WORDLE_ID.into();
-    assert!(!wordle.send(user_id, wordle_id).main_failed());
-    assert!(!game_session.send(user_id, wordle_id).main_failed());
+    assert!(!wordle.send(USER1, wordle_id).main_failed());
+    assert!(!game_session.send(USER1, wordle_id).main_failed());
     return sys;
 }
 
-// #[test]
+#[test]
 fn test_win() {
     let sys = setup();
     let game_session = sys.get_program(GAME_SESSION_ID).unwrap();
 
     // user1 starts game, and check words (the hidden word is "house" in test mode)
-    let user1: ActorId = USER1.into();
-    assert!(!game_session.send(user1, SessionAction::StartGame).main_failed());
-    assert!(!game_session.send(user1, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
-    assert!(!game_session.send(user1, SessionAction::CheckWord { word: "house".to_string() }).main_failed());
+    assert!(!game_session.send(USER1, SessionAction::StartGame).main_failed());
+    assert!(!game_session.send(USER1, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
+    assert!(!game_session.send(USER1, SessionAction::CheckWord { word: "house".to_string() }).main_failed());
 
     let state: State = game_session.read_state(b"").unwrap();
     assert_eq!(state.user_sessions.len(), 1);
-    assert_eq!(state.user_sessions[0].0, user1);
+    assert_eq!(state.user_sessions[0].0, USER1.into());
     assert_eq!(state.user_sessions[0].1.check_count, 2);
     assert_eq!(state.user_sessions[0].1.status, SessionStatus::StartGameWaiting);
     assert_eq!(state.user_sessions[0].1.result, SessionResult::Win);
@@ -43,13 +41,13 @@ fn test_win() {
     // support multiple users:
     // user2 starts game, and check words (the hidden word is "house" in test mode)
     let user2: ActorId = USER2.into();
-    assert!(!game_session.send(user2, SessionAction::StartGame).main_failed());
-    assert!(!game_session.send(user2, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
-    assert!(!game_session.send(user2, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
-    assert!(!game_session.send(user2, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
-    assert!(!game_session.send(user2, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
-    assert!(!game_session.send(user2, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
-    assert!(!game_session.send(user2, SessionAction::CheckWord { word: "house".to_string() }).main_failed());
+    assert!(!game_session.send(USER2, SessionAction::StartGame).main_failed());
+    assert!(!game_session.send(USER2, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
+    assert!(!game_session.send(USER2, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
+    assert!(!game_session.send(USER2, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
+    assert!(!game_session.send(USER2, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
+    assert!(!game_session.send(USER2, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
+    assert!(!game_session.send(USER2, SessionAction::CheckWord { word: "house".to_string() }).main_failed());
 
     let state: State = game_session.read_state(b"").unwrap();
     assert_eq!(state.user_sessions.len(), 2);
@@ -70,18 +68,17 @@ fn test_lose_with_too_many_check() {
     let game_session = sys.get_program(GAME_SESSION_ID).unwrap();
     
     // user starts game, and check word for >=6 times (the hidden word is "house" in test mode)
-    let user: ActorId = USER1.into();
-    assert!(!game_session.send(user, SessionAction::StartGame).main_failed());
-    assert!(!game_session.send(user, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
-    assert!(!game_session.send(user, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
-    assert!(!game_session.send(user, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
-    assert!(!game_session.send(user, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
-    assert!(!game_session.send(user, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
-    assert!(!game_session.send(user, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
+    assert!(!game_session.send(USER1, SessionAction::StartGame).main_failed());
+    assert!(!game_session.send(USER1, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
+    assert!(!game_session.send(USER1, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
+    assert!(!game_session.send(USER1, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
+    assert!(!game_session.send(USER1, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
+    assert!(!game_session.send(USER1, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
+    assert!(!game_session.send(USER1, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
 
     let state: State = game_session.read_state(b"").unwrap();
     assert_eq!(state.user_sessions.len(), 1);
-    assert_eq!(state.user_sessions[0].0, user);
+    assert_eq!(state.user_sessions[0].0, USER1.into());
     assert_eq!(state.user_sessions[0].1.check_count, 6);
     assert_eq!(state.user_sessions[0].1.status, SessionStatus::StartGameWaiting);
     assert_eq!(state.user_sessions[0].1.result, SessionResult::Lose);
@@ -93,16 +90,22 @@ fn test_lose_with_timeout() {
     let game_session = sys.get_program(GAME_SESSION_ID).unwrap();
     
     // user starts game, and check word for >=6 times (the hidden word is "house" in test mode)
-    let user: ActorId = USER1.into();
-    assert!(!game_session.send(user, SessionAction::StartGame).main_failed());
-    assert!(!game_session.send(user, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
+    assert!(!game_session.send(USER1, SessionAction::StartGame).main_failed());
+    assert!(!game_session.send(USER1, SessionAction::CheckWord { word: "human".to_string() }).main_failed());
 
-    sys.spend_blocks(10);
+    sys.spend_blocks(200);
 
     let state: State = game_session.read_state(b"").unwrap();
     assert_eq!(state.user_sessions.len(), 1);
-    assert_eq!(state.user_sessions[0].0, user);
+    assert_eq!(state.user_sessions[0].0, USER1.into());
     assert_eq!(state.user_sessions[0].1.check_count, 1);
     assert_eq!(state.user_sessions[0].1.status, SessionStatus::StartGameWaiting);
     assert_eq!(state.user_sessions[0].1.result, SessionResult::Lose);
+
+    let mailbox = sys.get_mailbox(USER1);
+    let log = Log::builder()
+        .source(GAME_SESSION_ID)
+        .dest(USER1)
+        .payload(SessionEvent::GameOver { result: SessionResult::Lose } );
+    assert!(mailbox.contains(&log));
 }
